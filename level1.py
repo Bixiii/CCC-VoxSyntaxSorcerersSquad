42703b7353e3ceae0278c6415c19a81046b06e0e
Nikolaus Kasyan <nikolaus.kasyan@voxtronic.com>
2023-10-20 15:12:54 +0200

    Solve level 1

--- level1.py
+++ level1.py
@@ -5,11 +5,21 @@ def process_file(input_path, output_path):
     with open(input_path, "r") as infile:
         for line in infile:
             input_data.append(line.strip())  # do some parsing here
-
+    map_size = int(input_data[0])
+    map = []
+    for i in range(map_size):
+        map.append(input_data[1+i])
+    numCoordinates = int(input_data[1+map_size])
+    coords = []
+    for i in range(numCoordinates):
+        coords.append([int(coordinateString) for coordinateString in input_data[2+map_size + i].split(",")])
+    print(map_size, map, numCoordinates, coords, sep="\n")
     results = input_data
     with open(output_path, "w") as outfile:
-        for result in results:
-            outfile.write(result + "\n")
+        for coord in coords:
+            x,y = coord
+            value = map[x][y]
+            outfile.write(f"{value}\n")
 
 
 def main():
